d385b6171a753557da2c144556139fd0e1095b2c
Mete Atamel <atamel@google.com>
2024-08-28 11:40:50 +0100

    Added function_calling_assistant.py

---
+++ python/function_calling_assistant.py
@@ -0,0 +1,63 @@
+import json
+import os
+
+from langchain import hub
+from langchain.agents import create_tool_calling_agent, AgentExecutor
+from langchain_core.tools import tool
+from langchain_google_vertexai import ChatVertexAI
+
+
+# Reference:
+# https://python.langchain.com/v0.2/docs/how_to/function_calling/
+# https://python.langchain.com/v0.1/docs/use_cases/tool_use/agents/
+
+@tool
+def get_weather_forecast(location: str) -> str:
+    """Get the weather forecast and temperature for a location"""
+    if location == "Paris":
+        return json.dumps({
+            "location": location,
+            "forecast": "sunny",
+            "temperature": 20
+        })
+    elif location == "London":
+        return json.dumps({
+            "location": location,
+            "forecast": "rainy",
+            "temperature": 15
+        })
+
+    return json.dumps({
+        "location": "unknown",
+        "forecast": "unknown",
+        "temperature": 0
+    })
+
+
+if __name__ == "__main__":
+    tools = [get_weather_forecast]
+
+    prompt = hub.pull("hwchase17/openai-tools-agent")
+    prompt.pretty_print()
+
+    llm = ChatVertexAI(
+        project=os.environ["PROJECT_ID"],
+        location="us-central1",
+        model="gemini-1.5-flash-001"
+    )
+
+    # Construct the tool calling agent
+    agent = create_tool_calling_agent(llm, tools, prompt)
+
+    # Create an agent executor by passing in the agent and tools
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    query = "What is the weather in Paris?"
+    print(f"User: {query}")
+    response = agent_executor.invoke({"input": query})
+    print(f"Response: {response['output']}")
+
+    query = "How's the weather in London vs. Paris?"
+    print(f"User: {query}")
+    response = agent_executor.invoke({"input": query})
+    print(f"Response: {response['output']}")
